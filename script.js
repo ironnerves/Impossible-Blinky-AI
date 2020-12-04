28b0d97904249320249cfc89ef5943aa87cf3b46
Someone On the Internet <ironnerves191@gmail.com>
2020-12-04 16:23:53 +0000

    I actually made the AI work. Sadly, the YOU WIN screen doesn't show up.

--- script.js
+++ script.js
@@ -4,39 +4,40 @@ let blinkyx
 let blinkyy
 let differencex
 let differencey
-function setup(){
-  createCanvas(400,400)
+function setup() {
+  createCanvas(400, 400)
   playerx = round(random(400))
   playery = round(random(400))
   blinkyx = round(random(400))
   blinkyy = round(random(400))
 }
 
-function draw(){
-  background(0,0,0)
-  fill(255,255,0)
-  rect(playerx,playery,50,50)
-  fill(255,0,0)
-  rect(blinkyx,blinkyy,50,50)
-  if(playerx - blinkyx == 0 && playery - blinkyy == 0){
-    fill(255,0,0)
-    text("YOU WIN",150,200)
-   }else{
-     differencex = playerx - blinkyx
-     differencey = playery - blinkyy
-     while(differencex ?? 0 & differencey ?? 0){
-       if(differencex < -1){
-        blinkyx = blinkyx + 1
-      }else if(differencex > 0) {
+function draw() {
+  background(0, 0, 0)
+  fill(255, 255, 0)
+  rect(playerx, playery, 50, 50)
+  fill(255, 0, 0)
+  rect(blinkyx, blinkyy, 50, 50)
+  differencex = playerx - blinkyx
+  differencey = playery - blinkyy
+  if (differencex === 0 && differencey === 0) {
+    fill(255, 0, 0)
+    textSize(50)
+    text("YOU WIN",50,50)
+  }else{
+    if (differencex != 0) {
+      if (differencex < -1) {
         blinkyx = blinkyx - 1
+      } else if (differencex > 0) {
+        blinkyx = blinkyx + 1
       }
-        if (differencey ?? 0) {
-          if (differencey < -1){
-           blinkyy = blinkyy + 1
-         }else if(differencey > 0){
-           blinkyy = blinkyy - 1
-         }
-       }
-     } 
-   }
+    }
+    if (differencey != 0) {
+      if (differencey < -1) {
+        blinkyy = blinkyy - 1
+      } else if (differencey > 0) {
+        blinkyy = blinkyy + 1
+      }
+    }
+  }
 }
