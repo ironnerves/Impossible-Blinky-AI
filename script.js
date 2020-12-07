42ce8f059cd8b2239a651af838ba69c51bd72e2a
Someone On the Internet <ironnerves191@gmail.com>
2020-12-07 14:49:51 +0000

    Added a hack button, it broke the entire program, and so I commented it out.

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 let playerx
 let playery
+let player_speed
 let blinkyx
 let blinkyy
+let blinky_speed
 let differencex
 let differencey
-let button1
+let reset_button
+// let hack_button
+// let hack_button_player_speed_descriptor
+// let hack_button_player_speed_input
 function setup() {
   createCanvas(400, 400)
+  noStroke()
   playerx = round(random(400))
   playery = round(random(400))
+  player_speed = 1
   blinkyx = round(random(400))
   blinkyy = round(random(400))
-  button1 = createButton("Click To Reset")
-  button1.mousePressed(reset_button)
+  blinky_speed = 1
+  reset_button = createButton("Click To Reset")
+  reset_button.mousePressed(reset_button_function)
+  // hack_button = createButton("Click To Enable Hacks")
+  // hack_button.mousePressed(hack_button_function)
 }
 
 function draw() {
@@ -28,38 +38,44 @@ function draw() {
     textSize(50)
     text("YOU LOSE", 50, 50)
   } else {
-    if (keyCode === UP_ARROW && keyIsPressed === true){
-      playery = playery - 1
+    if (keyCode === UP_ARROW && keyIsPressed === true) {
+      playery = playery - player_speed
     }
-    if (keyCode === DOWN_ARROW && keyIsPressed === true){
-      playery = playery + 1
+    if (keyCode === DOWN_ARROW && keyIsPressed === true) {
+      playery = playery + player_speed
     }
-    if(keyCode === LEFT_ARROW && keyIsPressed === true){
-      playerx = playerx - 1
+    if (keyCode === LEFT_ARROW && keyIsPressed === true) {
+      playerx = playerx - player_speed
     }
-    if(keyCode === RIGHT_ARROW && keyIsPressed === true){
-      playerx = playerx + 1
+    if (keyCode === RIGHT_ARROW && keyIsPressed === true) {
+      playerx = playerx + player_speed
     }
     if (differencex != 0) {
       if (differencex < -1) {
-        blinkyx = blinkyx - 1
+        blinkyx = blinkyx - blinky_speed
       } else if (differencex > 0) {
-        blinkyx = blinkyx + 1
+        blinkyx = blinkyx + blinky_speed
       }
     }
     if (differencey != 0) {
       if (differencey < -1) {
-        blinkyy = blinkyy - 1
+        blinkyy = blinkyy - blinky_speed
       } else if (differencey > 0) {
-        blinkyy = blinkyy + 1
+        blinkyy = blinkyy + blinky_speed
       }
     }
   }
 }
 
-function reset_button() {
-  playerx = round(random(255))
-  playery = round(random(255))
-  blinkyx = round(random(255))
-  blinkyy = round(random(255))
+function reset_button_function() {
+  playerx = round(random(400))
+  playery = round(random(400))
+  blinkyx = round(random(400))
+  blinkyy = round(random(400))
 }
+
+// function hack_button_function(){
+//   hack_button_player_speed_descriptor = createP("Type Your Desired Speed Of Pac-Man And Then Press Enter. It Also Turns You Invisible")
+//   hack_button_player_speed_input = createInput("1")
+//   player_speed = hack_button_player_speed_input.value()
+// }
